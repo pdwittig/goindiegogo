a666cc448db13b94690526381b5dbd1348279931
Phil Wittig <pdwittig@gmail.com>
2014-06-25 10:46:26 -0700

    generate rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goindiegogo::Application.config.secret_key_base = '36475fe14b44521bfd89db69ff66557d8a1bf7ae01721781b9bab399c7be77f515a06fc252c22db927205d1d041c66b53114c6f0458651948f91bdb394b7e77a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Goindiegogo::Application.config.session_store :cookie_store, key: '_goindiegogo_session'
